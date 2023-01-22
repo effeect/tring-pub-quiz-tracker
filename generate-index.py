7f7ead7bb3fb6fafcb30123f712f30e3e6fe183d
Oliver Dimes <oliver.dimes3@gmail.com>
2023-01-22 19:03:27 +0000

    Jinja Template working

--- generate-index.py
+++ generate-index.py
@@ -1,18 +1,26 @@
 from jinja2 import Template
 
-template = """
-<html>
-<head>
-<title>{{ title }}</title>
-</head>
-<body>
-<h1>{{ headline }}</h1>
-<p>{{ content }}</p>
-</body>
-</html>
-"""
+import pubdata
+from pubdata import *
 
-template = Template(template)
-output = template.render(title="My Title", headline="My Headline", content="My Content")
-print(output)
+csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQTlUMZ3zA543Gs4fHJvPvEO4Ld2xV_bQJyH5qmZ6X5melHeWa5nugeZXWA23acj0u5phl1E7OuTqL/pub?output=csv"
 
+template = "Error"
+# output = template.render(title="My Title", headline="My Headline", content="My Content")
+
+with open('index_template.html') as file_:
+    template = Template(file_.read())
+
+table_data = pubdata.get_sheets_data(csv_url)
+output = template.render(table=table_data)
+
+
+def publish_html(data):
+    with open("index.html", "w") as file_:
+        file_.write(data)
+        file_.close()
+
+
+if __name__ == "__main__":
+    publish_html(output)
+    print(output)
