524bf2808189b6094d0eee35135d3228612d7e92
Oliver Dimes <oliver.dimes3@gmail.com>
2023-02-12 20:03:03 +0000

    Added Kings Arm integration

--- generate-index.py
+++ generate-index.py
@@ -4,15 +4,16 @@ import pubdata
 from pubdata import *
 
 csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQTlUMZ3zA543Gs4fHJvPvEO4Ld2xV_bQJyH5qmZ6X5melHeWa5nugeZXWA23acj0u5phl1E7OuTqL/pub?output=csv"
+csv_url2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRw2kPIQF9YSrR6VJWuSwn95y9eP2oXUIrXZg7uvt8sE4ALLRhBAXTs2Pnhd5XnULZSRmWDJlIGjTOP/pub?output=csv"
 
-template = "Error"
-# output = template.render(title="My Title", headline="My Headline", content="My Content")
 
 with open('index_template.html') as file_:
     template = Template(file_.read())
 
 table_data = pubdata.get_sheets_data(csv_url)
-output = template.render(table=table_data)
+table_data2 = pubdata.get_sheets_data(csv_url2)
+
+output = template.render(table=table_data, table2=table_data2)
 
 
 def publish_html(data):
